9fa5411d08ae5c2e8d7164637ae099c7e14b0520
Seb <Seb@seb-six>
2013-03-07 20:58:43 +0100

    test

--- src/main/java/fr/seb/tempi/services/gpio/impl/GPIOServiceImpl.java
+++ src/main/java/fr/seb/tempi/services/gpio/impl/GPIOServiceImpl.java
@@ -3,13 +3,13 @@ package fr.seb.tempi.services.gpio.impl;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 
+import fr.seb.tempi.GPIOPinManager;
 import fr.seb.tempi.services.gpio.GPIOService;
 
 public class GPIOServiceImpl implements GPIOService {
 
 	public void writePinState(Pin pin, PinState state) {
-        
-
+		GPIOPinManager.getInstance().getPin(pin).setState(state);
 	}
 
 }
